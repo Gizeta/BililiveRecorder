e7937416f9537aba633080beb7acd1066353d47b
Gizeta <0w0@gizeta.tk>
2019-12-19 14:11:46 +0800

    feat: add console entrance

---
+++ BililiveRecorder.ConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using Autofac;
+using BililiveRecorder.Core;
+using BililiveRecorder.FlvProcessor;
+using NLog;
+
+namespace BililiveRecorder.ConsoleApp
+{
+    class Program
+    {
+        private static readonly Logger logger = LogManager.GetCurrentClassLogger();
+
+        static void Main(string[] args)
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterModule<FlvProcessorModule>();
+            builder.RegisterModule<CoreModule>();
+            var container = builder.Build();
+            var rootScope = container.BeginLifetimeScope("recorder_root");
+            var recorder = rootScope.Resolve<IRecorder>();
+            recorder.Initialize(".");
+            System.Console.ReadLine();
+        }
+    }
+}
